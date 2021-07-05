21245eae43ee3f9d309d0eaaa0bc38ed67826eb9
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-07-05 01:02:22 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
